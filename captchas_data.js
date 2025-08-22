7f2d31f1170841e8997086809c8f7abfaf7eeb19
Zacchon <46297604+Zacchon@users.noreply.github.com>
2025-08-22 20:30:36 +0300

    Make captchas_data.js

---
+++ captchas_data.js
@@ -0,0 +1,16 @@
+const captchasData = [
+    {   
+        id: 0,
+        imagePath: 'assets/scandalous.jpg',
+        rows: 3,
+        cols: 3,
+        prompt1: 'Select all images with',
+        prompt2: 'ankle',
+        correctAnswers: [
+            [0,0,0],
+            [1,1,0],
+            [0,0,0],
+        ],
+    },
+];
+        
